63a7024d21046498cd64a6c2dbf6980a8ca7e4a0
leen7awa <leen.hawa@e.braude.ac.il>
2024-09-18 19:25:32 +0300

    reset

--- front/src/Counter.jsx
+++ front/src/Counter.jsx
@@ -160,13 +160,12 @@ const Counter = ({ orders, setOrders }) => {
                 <ConfirmationModal
                     message={<span dir="rtl">לסיים את ההזמנה?</span>}
                     onConfirm={() => {
-                        // Remove the order from the list
+                        // Remove the order from the list and update local storage
                         setOrders(prevOrders => {
                             const updatedOrders = prevOrders.filter(order => order.orderNumber !== finishOrderItem.orderNumber);
+                            localStorage.setItem('orders', JSON.stringify(updatedOrders));
                             return updatedOrders;
                         });
-
-                        // Close the confirmation modal
                         setShowConfirmation(false);
                     }}
                     onCancel={() => setShowConfirmation(false)}
